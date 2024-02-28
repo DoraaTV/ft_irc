bf1c9960bd550e6186eb2ca2173529b3ea7b4a78
Doraa <tom.tayanne@yahoo.fr>
2024-02-28 09:26:37 +0100

    +

--- Commands/quit.cpp
+++ Commands/quit.cpp
@@ -6,7 +6,7 @@
 /*   By: parallels <parallels@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 15:14:15 by thrio             #+#    #+#             */
-/*   Updated: 2024/02/28 09:25:09 by parallels        ###   ########.fr       */
+/*   Updated: 2024/02/28 09:26:31 by parallels        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 void Server::quit(char *buffer, int clientSocket, std::deque<Client>::iterator senderClient) {
     static_cast<void>(buffer);
     static_cast<void>(clientSocket);
-    // leaves all channels he is in using broadcastMessage to send a message to all clients in the channel
     for (std::map<std::string, Channel*>::iterator it = _channels.begin(); it != _channels.end(); ++it) {
         if (!it->second)
             continue;
